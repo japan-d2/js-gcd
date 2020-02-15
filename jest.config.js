3d211b5e12e138d4f23491ebc0974a9e76946317
AOKI Yuuto <aoki@u-ne.co>
2020-02-15 22:38:29 +0900

    fix: wrong testMatch

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,6 @@
 module.exports = {
-  "roots": [
-    "<rootDir>/src"
-  ],
   "testMatch": [
-    "**/__tests__/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+    "<rootDir>/tests/**/*.test.ts"
   ],
   "transform": {
     "^.+\\.(ts|tsx)$": "ts-jest"
